44d7bf75bcb5d1121f18328613509ebf19129a6d
Sebastian Sanchez <sebastiansanchez@Sebastians-Mac-mini.local>
2022-06-22 14:48:55 -0500

    supported typescript with eslint and prettier

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+/**
+ * App - the main application react element
+ *
+ * @return {JSX.Element} the app react element
+ */
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>Personal Protafolio</p>
+			</header>
+		</div>
+	)
+}
+
+export default App
